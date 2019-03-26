a4bc223a79b9428891565cb49444b05d9900622a
Marta Demchyna <marta.demchyna@globallogic.com>
2019-03-26 11:46:06 +0200

    Homework - Selenium Easy site

--- src/test/java/New/Homework1.java
+++ src/test/java/New/Homework1.java
@@ -21,7 +21,7 @@ public class Homework1 {
     //private CSS checkBox;
 
     @Test
-    public void test1() throws InterruptedException {
+    public void inputForms() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().fullscreen();
@@ -48,6 +48,7 @@ public class Homework1 {
         driver.findElement(By.cssSelector("#gettotal > button")).click();
         String total = driver.findElement(By.id("displayvalue")).getText();
         Assert.assertEquals("10", total);
+
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(2) > a")).click();
         driver.findElement (By.cssSelector("#isAgeSelected")).click();
@@ -56,12 +57,15 @@ public class Homework1 {
         Thread.sleep(2000);
         driver.findElement (By.cssSelector("#check1")).click();
         Thread.sleep(2000);
+        //WebElement checkbox = driver.findElement(By.class("cb1-element"));
+        //System.out.println("Option1" + checkbox.isSelected());
         //WebElement checkedBox = findElementByClass("//li[@id='privileges:1']/div/span/div/div/span[@class='ui-tree-checkbox-icon.ui-icon.ui-icon-check']");
-        //assertTrue(checkedBox.isEnabled());
-        //div class= "checkbox";
+        //Assert.assertEquals(driver.findElement(By.cssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(3) > label"));
+        //Assert.assertFalse();
         //assertTrue(checkedBox.isEnabled());
         driver.findElement (By.xpath("//*[@id=\"check1\"]")).click();
         Thread.sleep(2000);
+
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(3) > a")).click();
         Thread.sleep(2000);
@@ -69,6 +73,7 @@ public class Homework1 {
         text = driver.findElement(By.xpath("//*[@id=\"easycont\"]/div/div[2]/div[1]/div[2]/p[3]")).getText();
         Assert.assertEquals("Radio button is Not checked", text);
         Thread.sleep(2000);
+
         driver.findElement (By.cssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(4) > div.panel-body > label:nth-child(2) > input[type=\"radio\"]")).click();
         driver.findElement (By.cssSelector("#buttoncheck")).click();
         Thread.sleep(2000);
@@ -85,6 +90,7 @@ public class Homework1 {
         Assert.assertEquals("Sex : Female\n" +
                 "Age group: 15 - 50", text);
         Thread.sleep(2000);
+
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(4) > a")).click();
         Thread.sleep(2000);
@@ -95,6 +101,7 @@ public class Homework1 {
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"printMe\"]")).click();
         Thread.sleep(2000);
+
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(5) > a")).click();
         driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/div/input")).clear();
@@ -115,6 +122,7 @@ public class Homework1 {
         driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[10]/div/div[2]/label/input")).click();
         driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[13]/div/button")).click();
         Thread.sleep(3000);
+
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
         driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(6) > a")).click();
         Thread.sleep(2000);
@@ -126,6 +134,18 @@ public class Homework1 {
         Thread.sleep(3000);
         text = driver.findElement(By.xpath("//*[@id=\"submit-control\"]")).getText();
         Assert.assertEquals("Form submited Successfully!", text);
+
+        driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > a")).click();
+        driver.findElement (By.cssSelector("#treemenu > li > ul > li:nth-child(1) > ul > li:nth-child(7) > a")).click();
+        driver.findElement (By.cssSelector("body > div.container-fluid.text-center > div > div.col-md-6.text-left > div:nth-child(2) > div > div.panel-body > span > span.selection > span")).click();
+        driver.findElement (By.cssSelector("#select2-country-results > li:nth-child(7)")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"select2-country-container\"]")).getText(), "Japan");
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div[2]/span/span[1]/span/ul/li/input")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div[2]/span/span[1]/span/ul/li/input")).sendKeys("Florida");
+        Thread.sleep(3000);
+        //Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div[2]/span/span[1]/span/ul/li/input")).getText(), "Florida");
+        driver.findElement(By.xpath("//*[@id=\"select2-fdsg-container\"]")).sendKeys("Puerto Rico");
         driver.close();
 
 
